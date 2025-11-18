f1e1c3d04984a85115c20f9dde8b894f98975d87
Aaaditya-03 <adityapaswan645@gmail.com>
2025-11-18 18:18:45 +0530

    Added assets and set-up dependencies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1 className= 'text-3xl text-primary'>
+        Gta-VI-Landing Page Using Gsap!!
+        </h1>
+      </div>
+      </main>
+  )
+}
+
+export default App
